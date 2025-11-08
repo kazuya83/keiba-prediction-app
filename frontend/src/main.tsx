d9e9b8d33397c29b567323169b5ea90ef4c6eadf
kazuya ishikawa <kzy83ishi15@gmail.com>
2025-11-09 02:02:58 +0900

    Refactor AuthProvider import in main.tsx to use specific AuthContext module for improved clarity and organization.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { AuthProvider } from './context'
+import { AuthProvider } from './context/AuthContext'
 import './index.css'
 import { startErrorMonitoring } from './utils/errorMonitor'
 import { errorHandler, handleUnhandledRejection } from './utils/errorHandler'
